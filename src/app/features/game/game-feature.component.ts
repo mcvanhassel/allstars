3126d15ad9cb0ea76a9097fbc145f7b1c40526da
Maurits van Hassel <mcvanhassel@gmail.com>
2020-06-20 10:47:05 +0200

    Add allstars roster to game component

--- src/app/features/game/game-feature.component.ts
+++ src/app/features/game/game-feature.component.ts
@@ -1,8 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { combineLatest, Observable } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+
+import { Player, PlayersService } from '../../core/domain/players';
+import { TeamsService } from '../../core/domain/teams';
+import { PlayerWithTeam } from './models';
 
 @Component({
   selector: 'allstars-game-feature',
   templateUrl: './game-feature.component.html',
   styleUrls: ['./game-feature.component.scss'],
 })
-export class GameFeatureComponent {}
+export class GameFeatureComponent implements OnInit {
+  players$: Observable<PlayerWithTeam[]> | undefined;
+
+  constructor(private readonly playersService: PlayersService, private readonly teamsService: TeamsService) {}
+
+  ngOnInit(): void {
+    this.players$ = this.getAllStarPlayers();
+  }
+
+  private getAllStarPlayers(): Observable<PlayerWithTeam[]> {
+    return this.playersService.getAllStars().pipe(switchMap(players => combineLatest(players.map(player => this.joinTeamName(player)))));
+  }
+
+  private joinTeamName(player: Player): Observable<PlayerWithTeam> {
+    return this.teamsService.getTeamById(player.teamId).pipe(map(team => ({ ...player, teamName: team?.name })));
+  }
+}
--- src/app/features/game/game-feature.module.ts
+++ src/app/features/game/game-feature.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { GameFeatureComponent } from './game-feature.component';
+import { RosterModule } from './modules/roster/roster.module';
 
 @NgModule({
   declarations: [GameFeatureComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RosterModule],
   exports: [GameFeatureComponent],
 })
 export class GameFeatureModule {}
