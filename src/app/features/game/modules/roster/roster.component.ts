2eff1db25cb1b34c8ff21f5811896436a3e1a09e
Maurits van Hassel <mcvanhassel@gmail.com>
2020-06-23 23:07:28 +0200

    Remove empty styles file

--- src/app/features/game/modules/roster/roster.component.ts
+++ src/app/features/game/modules/roster/roster.component.ts
@@ -7,7 +7,6 @@ import { PlayerWithTeam } from '../../models';
 @Component({
   selector: 'allstars-roster',
   templateUrl: './roster.component.html',
-  styleUrls: ['./roster.component.scss'],
 })
 export class RosterComponent implements OnInit {
   @Input() players$!: Observable<PlayerWithTeam[]>;
