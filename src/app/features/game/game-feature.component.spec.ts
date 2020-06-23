b67c2c519c10e111f4d717289a700f0b3a758c73
Maurits van Hassel <mcvanhassel@gmail.com>
2020-06-24 00:07:41 +0200

    Fix absolute import

--- src/app/features/game/game-feature.component.spec.ts
+++ src/app/features/game/game-feature.component.spec.ts
@@ -1,7 +1,7 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { AppConfigurationModule } from 'src/app/core/app-configuration';
 
+import { AppConfigurationModule } from '../../core/app-configuration';
 import { GameFeatureComponent } from './game-feature.component';
 import { RosterModule } from './modules/roster/roster.module';
 
