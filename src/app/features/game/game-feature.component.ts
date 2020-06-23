dd84933d4112660b1f1f3a37bec8d6d91120d89a
Maurits van Hassel <mcvanhassel@gmail.com>
2020-06-23 23:46:07 +0200

    Split western and eastern rosters

--- src/app/features/game/game-feature.component.ts
+++ src/app/features/game/game-feature.component.ts
@@ -3,28 +3,35 @@ import { combineLatest, Observable } from 'rxjs';
 import { map, shareReplay, switchMap } from 'rxjs/operators';
 
 import { Player, PlayersService } from '../../core/domain/players';
-import { TeamsService } from '../../core/domain/teams';
-import { PlayerWithTeam } from './models';
+import { Conference, TeamsService } from '../../core/domain/teams';
+import { BoxScoreWithPlayer, PlayerWithTeam } from './models';
 
 @Component({
   selector: 'allstars-game-feature',
   templateUrl: './game-feature.component.html',
+  styleUrls: ['./game-feature.component.scss'],
 })
 export class GameFeatureComponent implements OnInit {
-  players$!: Observable<PlayerWithTeam[]>;
+  westernPlayers$!: Observable<PlayerWithTeam[]>;
+  easternPlayers$!: Observable<PlayerWithTeam[]>;
+  westernBoxScores$!: Observable<BoxScoreWithPlayer[]>;
+  easternBoxScores$!: Observable<BoxScoreWithPlayer[]>;
 
   constructor(private readonly playersService: PlayersService, private readonly teamsService: TeamsService) {}
 
   ngOnInit(): void {
-    this.players$ = this.getAllStarPlayers();
-  }
+    const players$ = this.playersService.getAllStars().pipe(switchMap(this.joinTeams), shareReplay({ refCount: true }));
 
-  private getAllStarPlayers(): Observable<PlayerWithTeam[]> {
-    return this.playersService.getAllStars().pipe(switchMap(this.joinTeams), shareReplay({ refCount: true }));
+    this.westernPlayers$ = this.filterByConference(players$, Conference.West);
+    this.easternPlayers$ = this.filterByConference(players$, Conference.East);
   }
 
   private readonly joinTeams = (players: Player[]): Observable<PlayerWithTeam[]> => combineLatest(players.map(this.joinTeam));
 
   private readonly joinTeam = (player: Player): Observable<PlayerWithTeam> =>
     this.teamsService.getTeamById(player.teamId).pipe(map(team => ({ ...player, team })));
+
+  private filterByConference(players$: Observable<PlayerWithTeam[]>, conference: Conference): Observable<PlayerWithTeam[]> {
+    return players$.pipe(map(players => players.filter(player => player.team?.conference === conference)));
+  }
 }
